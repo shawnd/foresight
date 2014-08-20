2d5c283709a1b382cfa0277f2dde86b2c8a5b768
Costigan <ejc6@sfu.ca>
2014-08-20 16:43:33 -0700

    filter fields added

--- foresight/foresight_api/views.py
+++ foresight/foresight_api/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render
 from foresight_api.models import Users,Contact_Info,Room_Data
 from rest_framework import viewsets
 from foresight_api.serializers import Users_Serializer,Contact_Info_Serializer,Room_Data_Serializer
+from rest_framework import viewsets, filters, generics
 
 class Users_ViewSet(viewsets.ModelViewSet):
     queryset = Users.objects.all()
     serializer_class = Users_Serializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('RFID','firstName','lastName')
 
 class Contact_Info_ViewSet(viewsets.ModelViewSet):
 	queryset = Contact_Info.objects.all()
 	serializer_class = Contact_Info_Serializer
+	filter_backends = (filters.DjangoFilterBackend,)
+	filter_fields = ('user_id')
 
 class Room_Data_ViewSet(viewsets.ModelViewSet):
 	queryset = Room_Data.objects.all()
 	serializer_class = Room_Data_Serializer
+	filter_backends = (filters.DjangoFilterBackend,)
+	filter_fields = ('user_id')
